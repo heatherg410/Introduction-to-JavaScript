a0882b1c5ea0e7439c150ce39f2120bfdb59199a
Heather Colindres <callunaheather@Heathers-MacBook-Air.local>
2022-01-11 14:55:17 -0800

    solved task 5

--- index.js
+++ index.js
@@ -202,10 +202,10 @@ Using the miles function below do the following:
 3. Return the number of miles
 */
 
-function miles(/*add your code here*/){
-  /*add your code here*/
+function miles(km){
+  return km * 0.621371;
 }
-
+console.log(miles(2));
 
 
 //Task 5b - Feet to CM
@@ -216,11 +216,11 @@ Using the feet function below do the following:
 3. Return number of feet
 */
 
-function feet(/*add your code here*/){
-  /*add your code here*/
+function feet(cm){
+  return cm / 30.48;
 }
 
-
+console.log(feet(120));
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
